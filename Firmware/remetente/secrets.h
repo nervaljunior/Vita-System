4f40c3ee1f0c0dedefa4078dc0b39577630d647f
nervaljunior <nerval.junior@discente.ufma.br>
2023-10-22 11:26:11 -0300

    preparando codigo rev2

--- Firmware/remetente/secrets.h
+++ Firmware/remetente/secrets.h
@@ -3,8 +3,11 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
+/*
 #define SECRET_SSID "IEE";
-#define SECRET_PASS "iee@ufma.br";
+#define SECRET_PASS "iee@ufma.br";*/
+#define SECRET_SSID " DOMINICI";
+#define SECRET_PASS "99731540";
 #define SECRET_BROKER "broker.mqttdashboard.com"
 #define SECRET_PORT 1883
 #define SECRET_TOPIC "wokwi-weather"
