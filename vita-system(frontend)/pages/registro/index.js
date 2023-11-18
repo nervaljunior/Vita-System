fffcc15d1093111c3db6f890c8c259588fb5ea0c
nervaljunior <nerval.junior@discente.ufma.br>
2023-11-18 16:26:21 -0300

    cadastro de usuarios

--- vita-system(frontend)/pages/registro/index.js
+++ vita-system(frontend)/pages/registro/index.js
@@ -1,11 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Router from 'next/router';
+import styles from './Signup.module.css';
 
 const Signup = () => {
   const [signupError, setSignupError] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [nome, setNome] = useState('');
+  const [nasc, setNasc] = useState('');
+  const [natural, setNatural] = useState('');
+  const [cel, setCel] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [sexo, setSexo] = useState('Masculino');
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -17,6 +26,14 @@ const Signup = () => {
       body: JSON.stringify({
         email,
         password,
+        nome,
+        nasc,
+        natural,
+        cel,
+        telefone,
+        endereco,
+        cidade,
+        sexo,
       }),
     })
       .then((r) => r.json())
@@ -26,41 +43,127 @@ const Signup = () => {
         }
         if (data && data.token) {
           //set cookie
-          cookie.set('token', data.token, {expires: 2});
+          cookie.set('token', data.token, { expires: 2 });
           Router.push('/');
         }
       });
   }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <p>Sign Up</p>
-      <label htmlFor="email">
-        email
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          name="email"
-          type="email"
-        />
-      </label>
-
-      <br />
-
-      <label for="password">
-        password
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          name="password"
-          type="password"
-        />
-      </label>
-
-      <br />
-
-      <input type="submit" value="Submit" />
-      {signupError && <p style={{color: 'red'}}>{signupError}</p>}
-    </form>
+    <div className={styles.signupContainer}>
+      <header className={styles.header}>
+        <p>Formulário</p>
+      </header>
+      <div className={styles.formContainer}>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.formGroup}>
+            <label htmlFor="nome">Nome Completo</label>
+            <input
+              id="nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="email">E-mail</label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="nasc">Data de Nascimento</label>
+            <input
+              id="nasc"
+              type="date"
+              value={nasc}
+              onChange={(e) => setNasc(e.target.value)}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="natural">Naturalidade</label>
+            <input
+              id="natural"
+              value={natural}
+              onChange={(e) => setNatural(e.target.value)}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="cel">Celular</label>
+            <input
+              id="cel"
+              type="number"
+              value={cel}
+              onChange={(e) => setCel(e.target.value)}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="telefone">Telefone</label>
+            <input
+              id="telefone"
+              type="number"
+              value={telefone}
+              onChange={(e) => setTelefone(e.target.value)}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="endereco">Endereço</label>
+            <input
+              id="endereco"
+              value={endereco}
+              onChange={(e) => setEndereco(e.target.value)}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="cidade">Cidade</label>
+            <input
+              id="cidade"
+              value={cidade}
+              onChange={(e) => setCidade(e.target.value)}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Sexo</label>
+            <div className={styles.radioGroup}>
+              <label>
+                <input
+                  type="radio"
+                  value="Masculino"
+                  checked={sexo === 'Masculino'}
+                  onChange={() => setSexo('Masculino')}
+                />
+                Masculino
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="Feminino"
+                  checked={sexo === 'Feminino'}
+                  onChange={() => setSexo('Feminino')}
+                />
+                Feminino
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="Outro"
+                  checked={sexo === 'Outro'}
+                  onChange={() => setSexo('Outro')}
+                />
+                Outro
+              </label>
+            </div>
+          </div>
+          <div className={styles.formGroup}>
+            <button type="submit" className={styles.submitButton}>
+              Cadastrar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
